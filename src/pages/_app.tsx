b3a47665fafca62099d1cf31c9ce7853581523bf
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-04-21 08:52:29 -0300

    day 02 done

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { Header } from "../components/Header";
+import { Player } from "../components/Player";
+
+import styles from "../styles/app.module.scss";
+import "../styles/global.scss";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
+}
+
+export default MyApp;
