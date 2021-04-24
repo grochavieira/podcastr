46da7a046d580676a2e301a91438c80371f7ab56
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-04-24 09:42:36 -0300

    deploy setup

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3333/";
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:3333/";
+console.log(process.env.REACT_APP_API_URL);
 
 export const api = axios.create({
   baseURL,
