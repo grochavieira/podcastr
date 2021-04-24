e2fa4a18698d7838662ad91cc494f8bee57fec0b
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2021-04-24 09:34:39 -0300

    added fake api url by .env file

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3333/";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3333/",
+  baseURL,
 });
